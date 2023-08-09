bd36e0046be05afc520d2aedc321df60e3f0f61f
Yan Lyra <yanlyra3@gmail.com>
2023-08-09 20:37:00 -0300

    chore: created upload file input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Input } from '@/components/Input'
 import { SettingsTabs } from '@/components/SettingsTabs'
-import { Mail } from 'lucide-react'
+import { Mail, UploadCloud, User } from 'lucide-react'
 
 export default function Home() {
   return (
@@ -88,7 +88,30 @@ export default function Home() {
               </span>
             </label>
 
-            <div />
+            <div className="flex items-start gap-5">
+              <div className="flex h-16 w-16 shrink-0 items-center justify-center rounded-full bg-violet-50">
+                <User className="h-8 w-8 text-violet-500" />
+              </div>
+
+              <label className="group flex flex-1 cursor-pointer flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-center text-zinc-500 shadow-sm transition-colors duration-200 hover:border-violet-200 hover:bg-violet-25 hover:text-violet-500">
+                <div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 transition-colors duration-200 group-hover:border-violet-50 group-hover:bg-violet-100">
+                  <UploadCloud className="h-5 w-5 text-zinc-600 transition-colors duration-200 group-hover:text-violet-600" />
+                </div>
+
+                <div className="flex flex-col items-center gap-1">
+                  <span className="text-sm">
+                    <span className="font-semibold text-violet-700">
+                      Click to upload
+                    </span>{' '}
+                    or drag and drop
+                  </span>
+                  <span className="text-xs">
+                    SVG, PNG, JPG or GIF (max. 800x400px)
+                  </span>
+                </div>
+                <input type="file" className="sr-only" id="photo" />
+              </label>
+            </div>
           </div>
 
           <div className="grid grid-cols-form gap-3 pt-5">
