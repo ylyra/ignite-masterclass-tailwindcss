8c2a826f2d88a9c34cdc6be54b1826bae1727f21
Yan Lyra <yanlyra3@gmail.com>
2023-08-08 23:27:41 -0300

    chore: added logo and search input

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,9 +1,21 @@
-import Image from 'next/image'
+import { Search } from 'lucide-react'
+import { Logo } from './Logo'
 
 export function Sidebar() {
   return (
-    <aside className="border-r border-zinc-200 px-4 py-8">
-      <Image src="/logo.svg" width={142} height={32} alt="" />
+    <aside className="flex flex-col space-y-6 border-r border-zinc-200 px-4 py-8">
+      <strong className="flex items-center space-x-2 font-mono text-xl font-semibold text-zinc-900">
+        <Logo />
+        <span>Untitled UI</span>
+      </strong>
+
+      <div className="flex items-center space-x-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm focus-within:border-purple-300 focus-within:ring focus-within:ring-purple-100">
+        <Search className="h-5 w-5 text-zinc-500" />
+        <input
+          placeholder="Search"
+          className="flex-1 p-0 text-zinc-900 placeholder-zinc-600 outline-transparent"
+        />
+      </div>
     </aside>
   )
 }
