70609304c6092dcc9b03effea479908c6bfb5638
Yan Lyra <yanlyra3@gmail.com>
2023-08-10 21:21:05 -0300

    chore: fixed fileitem class order

--- src/components/Form/FileInput/FileItem.tsx
+++ src/components/Form/FileInput/FileItem.tsx
@@ -62,17 +62,17 @@ export function FileItem({ state, name, size }: FileItemProps) {
       {state === 'error' ? (
         <div className="flex flex-1 flex-col items-start gap-1">
           <div className="flex flex-col leading-relaxed">
-            <span className="text-error-700 dark:text-error-400 text-sm font-medium">
+            <span className="text-sm font-medium text-error-700 dark:text-error-400">
               Upload failed, please try again
             </span>
-            <span className="text-error-600 dark:text-error-500 text-sm">
+            <span className="text-sm text-error-600 dark:text-error-500">
               {name}
             </span>
           </div>
 
           <button
             type="button"
-            className="text-error-700 hover:text-error-900 dark:text-error-400 dark:hover:text-error-300 text-sm font-semibold"
+            className="text-sm font-semibold text-error-700 hover:text-error-900 dark:text-error-400 dark:hover:text-error-300"
           >
             Try again
           </button>
