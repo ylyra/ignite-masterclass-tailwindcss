50b811691bc57a34b37986e1d95b7aa024254a69
Yan Lyra <yan.lyra@growth.sale>
2023-08-20 19:13:27 -0300

    chore: finished project

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -19,9 +19,10 @@ export default function Home() {
       <div className="mt-6 flex flex-col">
         <div className="flex flex-col justify-between gap-4 border-b border-zinc-200 pb-5 dark:border-zinc-700 lg:flex-row lg:items-center">
           <div className="space-y-1">
-            <h2 className="text-lg font-medium text-zinc-900 dark:text-zinc-400">
+            <h2 className="text-lg font-medium text-zinc-900 dark:text-zinc-100">
               Personal info
             </h2>
+
             <span className="text-sm text-zinc-500">
               Update your photo and personal details here.
             </span>
